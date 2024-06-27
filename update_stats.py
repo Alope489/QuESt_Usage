0be43418fa7b39b92c2123c2bdb0586a1e2635f5
Alope489 <114271327+Alope489@users.noreply.github.com>
2024-06-27 07:41:56 -0600

    Update update_stats.py

--- update_stats.py
+++ update_stats.py
@@ -66,7 +66,7 @@ def save_to_json(data, filename):
 def main():
     repo_owner = "sandialabs"
     repo_name = "snl-quest"
-    access_token = "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"
+    access_token = os.getenv("QUEST_TOKEN")
     try:
         download_stats = get_github_downloads(repo_owner, repo_name, access_token)
         save_to_json(download_stats, "downloads.json")
